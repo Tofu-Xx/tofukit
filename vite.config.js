acfaff1079b66a0f729eb5a3331021095c1a77c5
Tofu <1714843907@qq.com>
2024-08-10 07:21:15 +0800

    feat:test

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import csso from "postcss-csso";
 
 export default defineConfig({
   base: "/",
+  css: {
+    postcss: {
+      plugins: [
+        csso(), // 压缩 CSS
+      ],
+    },
+  },
   build: {
     lib: {
-      entry: "src/main.js",
+      entry: resolve(__dirname, 'src/main.js'),
       name: "tofukit",
-      fileName: (format) => `main.${format}.js`
+      fileName: (format) => `main.${format}.js`,
+      formats: ["es"]
     }
-  }
+  },
 });
