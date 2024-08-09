f1059bbef440c579597ecddc1f2d632fd7a9bf9b
Tofu <1714843907@qq.com>
2024-08-10 07:29:52 +0800

    chore:cancel css config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import csso from "postcss-csso";
 
 export default defineConfig({
   base: "/",
-  css: {
-    postcss: {
-      plugins: [
-        // csso(), // 压缩 CSS
-      ],
-    },
-  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.js'),
