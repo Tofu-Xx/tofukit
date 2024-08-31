766de1d6420a73696b64de5ac8f8dbabdd45709d
Tofu <1714843907@qq.com>
2024-08-31 21:34:36 +0800

    feat: hij

--- packages/cij/src/index.ts
+++ packages/cij/src/index.ts
@@ -1,10 +1,24 @@
-export const cij = ({ raw }, ...args: any[]) => {
-  const cssString = raw.reduce(
+const _handle = (raw: TemplateStringsArray, vars: any[]) =>
+  raw.reduce(
     (result: string, part: string, i: number) =>
-      result + part + (args[i] ??= ""),
+      result + part + (vars[i] ??= ""),
     "",
   );
+export const cij = ({ raw }, ...args: any[]) => {
+  const cssString = _handle(raw, args);
   const style = document.createElement("style");
   style.textContent = cssString;
   document.querySelector("head")?.appendChild(style);
 };
+
+export const hij = ({ raw }, ...args: any[]) => ({
+  mount: (select: string) => {
+    document.querySelector(select)!.innerHTML = _handle(raw, args);
+  },
+  // const parser = new DOMParser();
+  // const doc = parser.parseFromString(`<hij>${htmlString}</hij>`, "text/html")
+  //   .querySelector(
+  //     "hij",
+  //   )!.children;
+  // return doc.length > 1 ? Array.from(doc) : doc[0];
+});
