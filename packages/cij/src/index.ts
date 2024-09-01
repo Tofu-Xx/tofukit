0d17d62e0c4975c2a8265923772e9d265b449863
Tofu <1714843907@qq.com>
2024-09-01 10:48:59 +0800

    chore: release v0.0.11

--- packages/cij/src/index.ts
+++ packages/cij/src/index.ts
@@ -10,15 +10,3 @@ export const cij = ({ raw }, ...args: any[]) => {
   style.textContent = cssString;
   document.querySelector("head")?.appendChild(style);
 };
-
-// export const hij = ({ raw }, ...args: any[]) => ({
-//   mount: (select: string) => {
-//     document.querySelector(select)!.innerHTML = _handle(raw, args);
-//   },
-  // const parser = new DOMParser();
-  // const doc = parser.parseFromString(`<hij>${htmlString}</hij>`, "text/html")
-  //   .querySelector(
-  //     "hij",
-  //   )!.children;
-  // return doc.length > 1 ? Array.from(doc) : doc[0];
-// });
