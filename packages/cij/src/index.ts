a5d9d3147f084938e23726f2ff5bb7a5ac60dc77
Tofu-Xx <1714843907@qq.com>
2024-10-21 22:21:12 +0800

    style: update

--- packages/cij/src/index.ts
+++ packages/cij/src/index.ts
@@ -3,10 +3,10 @@ const _handle = (raw: TemplateStringsArray, vars: any[]) =>
     (result: string, part: string, i: number) =>
       result + part + (vars[i] ??= ""),
     "",
-  );
+  )
 export const cij = ({ raw }, ...args: any[]) => {
   const cssString = _handle(raw, args);
   const style = document.createElement("style");
   style.textContent = cssString;
   document.querySelector("head")?.appendChild(style);
-};
+}
